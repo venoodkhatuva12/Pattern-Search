eb306f93321e952272c70f474b862b1c7841db3a
Venood Katuwa <venood.khatuva12@gmail.com>
2016-08-28 15:26:14 +0530

    Update patternsrc.sh

--- patternsrc.sh
+++ patternsrc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Script for installing swatch.
+#Script for Pattern search...
 #Author: Vinod.N K
 #Usage: Malware or Unknown Pattern search
 #Distro : Linux -Centos, Rhel, and any fedora
@@ -8,8 +8,8 @@ if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-
-PATTERNS=r57sh c99sh h00ly 0wn3d rst.void.ru
+echo "please put your patter after patters which you like to search..."
+PATTERNS=r57sh c99sh h00ly 0wn3d rst.void.ru ##you can put your own patter if you see any...
 PATTERNS_FINAL=$(echo $PATTERNS | sed 's/  /|/g')
 find /home/*/public_html/ -type f -exec grep -Eil "$PATTERNS_FINAL" {} ; >> /root/scanresult.txt
 
